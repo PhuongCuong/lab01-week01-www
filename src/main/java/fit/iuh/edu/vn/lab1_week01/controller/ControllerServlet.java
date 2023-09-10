0093ddd7b96423068e19a3a8f1cfa89f36ea6e52
PhuongCuong <20078421.cuong@student.iuh.edu.vn>
2023-09-10 08:55:26 +0700

    new project jakartarEE

--- src/main/java/fit/iuh/edu/vn/lab1_week01/controller/ControllerServlet.java
+++ src/main/java/fit/iuh/edu/vn/lab1_week01/controller/ControllerServlet.java
@@ -1,4 +1,22 @@
 package fit.iuh.edu.vn.lab1_week01.controller;
 
-public class ControllerServlet{
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/control","/controlSerlet"})
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,9 +5,8 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<form action="control">
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
